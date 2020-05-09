025d197ab06851059670a3bb95233d2a7348a9a3
Barry O'Driscoll <sc19bod@leeds.ac.uk>
2020-05-09 18:08:07 +0100

    firebase config and inital login

---
+++ app/src/main/java/uni/leeds/comp3222/LoginActivity.kt
@@ -0,0 +1,24 @@
+package uni.leeds.comp3222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+}
