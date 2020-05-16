da5a3af5b4313d1a020c510cd686f95245366cd0
Barry O'Driscoll <sc19bod@leeds.ac.uk>
2020-05-16 17:06:21 +0100

    integrated algolia searching

--- app/src/main/java/uni/leeds/comp3222/SearchActivity.kt
+++ app/src/main/java/uni/leeds/comp3222/SearchActivity.kt
@@ -4,12 +4,18 @@ import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.algolia.search.saas.AlgoliaException
+import com.algolia.search.saas.Client
+import com.algolia.search.saas.CompletionHandler
+import com.algolia.search.saas.Query
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_search.*
+import org.json.JSONArray
+import org.json.JSONObject
 import uni.leeds.recycler.SearchAdapter
 
 
@@ -59,10 +65,38 @@ class SearchActivity : AppCompatActivity() {
         
     }
 
-    private fun performSearch(query: String) {
+    private fun performSearch(queryString: String) {
 
-        firestore.collection("listings")
-            .whereEqualTo("itemName", query)
+        var client:Client = Client(getString(R.string.algolia_app_id),
+            getString(R.string.algolia_search_api_key))
+
+        var index = client.getIndex("listings")
+
+        var query: Query = Query(queryString)
+            .setAttributesToRetrieve("*")
+            .setHitsPerPage(50)
+
+        index.searchAsync(query, object : CompletionHandler {
+            override fun requestCompleted(content: JSONObject?, error: AlgoliaException?) {
+                var hits: JSONArray = content!!.getJSONArray("hits")
+                for (i in 0 until hits.length()) {
+                    val hit = hits.getJSONObject(i)
+
+                    var listing = firestore.collection("listings")
+                        .document(hit.getString("objectID")).get()
+                        .addOnSuccessListener { document ->
+                            val listing = document.toObject(Listing::class.java)!!
+                            listings.add(listing)
+                            adapter.notifyItemChanged(listings.size - 1)
+                        }
+
+                }
+            }
+        })
+
+
+        /*firestore.collection("listings")
+            .whereEqualTo("itemName", queryString)
             .get()
             .addOnSuccessListener { documents ->
                 for (document in documents) {
@@ -73,7 +107,7 @@ class SearchActivity : AppCompatActivity() {
             }
             .addOnFailureListener { exception ->
                 //TODO handle failure to load data
-            }
+            }*/
 
     }
 
