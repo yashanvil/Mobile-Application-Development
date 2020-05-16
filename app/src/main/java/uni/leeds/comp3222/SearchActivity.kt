aebb1d70d3ea5307d5ef040d8c3816c8734b0069
Barry O'Driscoll <sc19bod@leeds.ac.uk>
2020-05-16 03:49:17 +0100

    search working with exact text match

--- app/src/main/java/uni/leeds/comp3222/SearchActivity.kt
+++ app/src/main/java/uni/leeds/comp3222/SearchActivity.kt
@@ -1,13 +1,16 @@
 package uni.leeds.comp3222
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
-import uni.leeds.recycler.SearchAdapter
 import kotlinx.android.synthetic.main.activity_search.*
+import uni.leeds.recycler.SearchAdapter
 
 
 class SearchActivity : AppCompatActivity() {
@@ -33,7 +36,45 @@ class SearchActivity : AppCompatActivity() {
 
         searchResultsList.adapter = adapter
 
-        loadListings()
+        clearResults.setOnClickListener{
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchField.apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            setIconifiedByDefault(false)
+        }
+
+        // Verify the action and get the query
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                performSearch(query)
+                clearResults.visibility = View.VISIBLE
+            }
+        } else {
+            loadListings()
+        }
+        
+    }
+
+    private fun performSearch(query: String) {
+
+        firestore.collection("listings")
+            .whereEqualTo("itemName", query)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    val listing = document.toObject(Listing::class.java)
+                    listings.add(listing)
+                    adapter.notifyItemChanged(listings.size - 1)
+                }
+            }
+            .addOnFailureListener { exception ->
+                //TODO handle failure to load data
+            }
+
     }
 
     private fun loadListings() {
