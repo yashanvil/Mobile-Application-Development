92864e0683945952f9ba9ac6feee13bce637c8d1
Barry O'Driscoll <sc19bod@leeds.ac.uk>
2020-05-17 20:32:01 +0100

    final style fixes, added app icon and label, adjusted toolbar/hamburger colors

--- app/src/main/java/uni/leeds/comp3222/SearchActivity.kt
+++ app/src/main/java/uni/leeds/comp3222/SearchActivity.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
